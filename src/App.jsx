0197bc3fbc751b78017409f4c4e2f8def9a448b7
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-16 18:44:31 +0900

    48ch ok

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,35 @@ import { useState } from "react";
 
 const App = () => {
   let [ formObj,setFormObj ] = useState({fName: '',lName: '',city: '',gender: ''})
+
+  const inputOnChange = (property,value) => {
+    setFormObj(preObj => ({
+      ...preObj,
+      [property]:value
+    }))
+  }
  
+  const formSubmit = (e) => {
+    e.preventDefault();
+    console.log(formObj);
+    
+  }
+
   return (
     <div className="container"> 
       <form action="">
-        <input type="text" placeholder="First Name" value={formObj.fName}/>
-        <input type="text" placeholder="Last Name"  value={formObj.lName}/>
-        <select>
+        <input onChange={(e)=>{inputOnChange('fName',e.target.value)}} type="text" placeholder="First Name" value={formObj.fName}/>
+        <input onChange={(e)=>{inputOnChange('lName',e.target.value)}} type="text" placeholder="Last Name"  value={formObj.lName}/>
+        <select onChange={(e)=>{inputOnChange('city',e.target.value)}} value={formObj.city}>
           <option value="">Chose City</option>
           <option value="Rajuary">Rajuary</option>
           <option value="Dacka">Dacka</option>
         </select>
 
-        <input checked={formObj.gender === 'Male'} type="radio"  name="gender"/>Male
-        <input checked={formObj.gender === 'Female'} type="radio"  name="gender"/>Female
+        <input onChange={()=>{inputOnChange('gender','Male')}} checked={formObj.gender === 'Male'} type="radio"  name="gender"/>Male
+        <input onChange={()=>{inputOnChange('gender','Female')}} checked={formObj.gender === 'Female'} type="radio"  name="gender"/>Female
         <br />
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={formSubmit}>Submit</button>
       </form>
     </div>
   );
