6a8eaec77ba7658e951f55671497ba1096af1357
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-16 18:14:48 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import { useState } from "react";
 
 
 const App = () => {
-
+  let [ formObj,setFormObj ] = useState({fName: '',lName: '',city: '',gender: ''})
  
   return (
     <div className="container"> 
       <form action="">
-        <input type="text" placeholder="First Name"/>
-        <input type="text" placeholder="Last Name"/>
+        <input type="text" placeholder="First Name" value={formObj.fName}/>
+        <input type="text" placeholder="Last Name"  value={formObj.lName}/>
         <select>
           <option value="">Chose City</option>
           <option value="Rajuary">Rajuary</option>
           <option value="Dacka">Dacka</option>
         </select>
 
-        <input type="radio"  name="gender"/>Male
-        <input type="radio"  name="gender"/>Female
+        <input checked={formObj.gender === 'Male'} type="radio"  name="gender"/>Male
+        <input checked={formObj.gender === 'Female'} type="radio"  name="gender"/>Female
         <br />
         <button type="submit">Submit</button>
       </form>
